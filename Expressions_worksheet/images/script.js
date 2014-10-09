d156bb3143ad932a17bd535a2780994cd11e31b4
bowersk <kdbowers@fullsail.edu>
2014-10-09 16:55:41 -0400

    Edit assoc. for css page and completed dog problem

--- Expressions_worksheet/images/script.js
+++
@@ -1,7 +0,0 @@
-/*
-Keila Bowers
-WDD144-0 Section 02
-Expressions Worksheet
-October 9, 2014
- */
-
---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,16 @@
+/*
+Keila Bowers
+WDD144-0 Section 02
+Expressions Worksheet
+October 9, 2014
+ */
+
+//Dog Years - Calculate how old Sparky is in dog years based on his actual age.
+var ageYears = 2;
+var dogYears = 7;
+
+//Multiply current age in years by 7 dog years to get current age in dog years
+var ageDogYears = ageYears * dogYears;
+
+//print to console the output of current age in dog years.
+console.log("Sparky is " + ageYears + " human years old which is " + ageDogYears + " in dog years.")
