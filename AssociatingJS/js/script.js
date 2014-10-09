c17a4fc96c78c1206e93f1440330d14688a10433
bowersk <kdbowers@fullsail.edu>
2014-10-09 14:51:56 -0400

    Screencast: Order of Operations
    PEMDAS

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,6 +1,20 @@
 //JavaScript
-//Expressions - Modulo Operator  % find the reminder
+//Order of Operations PEMDAS
 
-var remainder = 9 % 2;
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
 
-console.log(remainder);
+//the average is the sum of the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4) /4;
+
+//console.log(average);
+
+
+var lenght = 7;
+var width = 6;
+var perimeter = (lenght * 2) + (width * 2);//not needed ( )
+
+console.log(perimeter);
