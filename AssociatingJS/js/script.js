dbdd6e42749cf9cf63782dd2ace6552bfa8b5bf2
bowersk <kdbowers@fullsail.edu>
2014-10-09 13:45:30 -0400

    Screencast: Using Console.log() and Alert()
    Using Alerts (Pop Up Boxes) and printing to the console with
    console.log()

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,11 @@
-alert("This is in the js file");
+//JavaScript
+
+var yearBorn = 1955;
+
+//Printing to the console
+/*
+console.log("Hello World!");
+console.log(yearBorn);
+    */
+
+alert(yearBorn);
