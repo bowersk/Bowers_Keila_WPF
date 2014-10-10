954ec92a1c8bb7158bdd22ba59f49502eea3d0bb
bowersk <kdbowers@fullsail.edu>
2014-10-09 22:13:27 -0400

    Enter prompt text for user provided vars

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,9 +8,9 @@
 //Figure out keyword density for search engine optimization.
 
 alert()
-var phrasePresence = prompt();
-var phraseWordCount = prompt();
-var wordCountTotal = prompt();
+var phraseWordCount = prompt("How many words are in your keyword phrase?");
+var phrasePresence = prompt("How many times is your phrase shown in the text?");
+var wordCountTotal = prompt("Please enter the total word count for your article.");
 
 var phraseDensity = ();
 
