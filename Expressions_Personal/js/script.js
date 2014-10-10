250e3107de0f9763ffd0a6f5e6cca4fdbba79364
bowersk <kdbowers@fullsail.edu>
2014-10-09 20:35:31 -0400

    Created var and formulas

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,3 +5,14 @@
  October 9, 2014
  */
 
+//Figure out how much it will cost to consume pop over a time frame.
+
+var name = prompt("Please enter your name:");
+var popDaily = prompt("How many cans of pop do you drink daily?");
+var popDays = prompt("How many days would you like to budget for?");
+var popCostPack = prompt("How much does it currently cost for a twelve pack of your pop?");
+
+var popCostSingle = popCostPack / 12;
+var popTotal = popCostSingle * popDaily * popDays;
+
+console.log(name + ", if you continue to drink " + popDaily + " cans of pop per day, for " + popDays + " days, it will cost you a total of $" + popTotal + ".");
