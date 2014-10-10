259fb29b47d501728c8ae932f3156c56bff9bd24
bowersk <kdbowers@fullsail.edu>
2014-10-09 22:18:13 -0400

    created result formula and checked in browser

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,7 +12,7 @@ var phraseWordCount = prompt("How many words are in your keyword phrase?");
 var phrasePresence = prompt("How many times is your phrase shown in the text?");
 var wordCountTotal = prompt("Please enter the total word count for your article.");
 
-var phraseDensity = ();
+var phraseDensity = phrasePresence * phraseWordCount / wordCountTotal * 100;
 
-console.log();
+console.log(phraseDensity);
 
