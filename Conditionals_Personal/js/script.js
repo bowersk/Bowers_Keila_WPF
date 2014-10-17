3ac74240b1dfd83da14edaabde3fd5a85d25bf35
bowersk <kdbowers@fullsail.edu>
2014-10-16 23:08:52 -0400

    Defined Var and checked console.log

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,3 +7,14 @@
 
 //Figure out what gauge of cable to use based on electrical current.
 
+alert("Welcome, we will figure out what gauge of cable you need based on current load. \nWe will first need to determine current.");
+
+var watts = prompt("Please enter wattage.");
+var volts = prompt("Please enter voltage.");
+var current = (watts / volts);
+
+
+
+console.log(watts);
+console.log(volts);
+console.log(current);
