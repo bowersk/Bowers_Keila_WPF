bde0cdcfc818a97bad4c4954bcbbe6dae39bcaab
bowersk <kdbowers@fullsail.edu>
2014-10-23 10:50:46 -0400

    Anonymous Functions notes

--- Week_4/js/script.js
+++ Week_4/js/script.js
@@ -1,12 +1,24 @@
 //JavaScript
-//ScreenCast - Procedures
+//ScreenCast - Anonymous Functions
 
 /*
-Function returns values
-Procedure does not return values
+also called closures
+
+
  */
 
 
+//more common structure
+var calcArea = function(width, height){
+   var area = width * height;
+    return area;
+
+}
+
+var a = calcArea(20, 30);
+console.log(a);
+
+//downside have to be declared before they are called
 
 
-
+//anoy is for code brevity and better scope
