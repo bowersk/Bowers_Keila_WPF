d692b2594aca04474e9ce0448a85302f2877ca59
bowersk <kdbowers@fullsail.edu>
2014-10-23 10:18:06 -0400

    Intro to Function notes

---
+++ Week_4/js/script.js
@@ -0,0 +1,16 @@
+//JavaScript
+//ScreenCast - Intro to Functions Reusable
+
+/* Why do we need functions?
+    DRY - Don't Repeat Yourself
+
+    Functions store stuff
+        functions store lines of code
+
+    one instance that is referenced over and over again
+    They organize code
+ */
+
+
+
+
