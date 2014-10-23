04fd6f45eaa61db84dbbc105ac0fe406ec5bd4de
bowersk <kdbowers@fullsail.edu>
2014-10-23 10:43:44 -0400

    returning values notes

--- Week_4/js/script.js
+++ Week_4/js/script.js
@@ -1,40 +1,16 @@
 //JavaScript
-//ScreenCast - arguments and parameters
-
-/*
-The structure
-    function functionName() {
-    }
-
-Arguments - Call
-    funcName(argument1);
-Parameter - define
-
- */
-
-/*
-//item being stored - argument
-calcArea(30, 20)
+//ScreenCast - returning values (finished values)
 
 
-//storage bins - parameter
+var total = calcArea(30, 20);
+
+
 function calcArea(w, h){
     var area = w * h;
-    console.log(area);
-}
-*/
-
-
-function dogYears(age){
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + "years old.");
+    return area; //function spitting the info out
 }
 
-//number we pass in ()
-
-var age1 = 4;
-dogYears(age1);
-dogYears(5);
+console.log(total);
 
 
 
