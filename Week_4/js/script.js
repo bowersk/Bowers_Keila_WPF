75b1314a2f6c716fb6d89482c8808003b166ad18
bowersk <kdbowers@fullsail.edu>
2014-10-23 14:45:24 -0400

    Week 4 GTT notes

--- Week_4/js/script.js
+++ Week_4/js/script.js
@@ -1,13 +1,51 @@
 //JavaScript
-//ScreenCast - For loop
+//Weekly GTT Video
 
+/*
+the 4 types: Sequential(Expressions); Conditional(If Statements, Ternaries); Repetitive(loops); Reusable (Functions);
 
-//shorthand version of the loops - combination of the loops
+ */
 
-for (var i = 10; i > 0; i--);{
-    console.log(i + "kegs on the wall")
+var min = prompt("please type in a min number");
+
+/*
+//will keep prompting until it is entered!
+while(min==""){
+   min = prompt("please enter a number!!")
 }
 
+//not a Number: NaN, checks to see if the value is a number
+while(isNaN(min)){
+    min = prompt("please enter a number");
+}
+*/
 
-//break; jumps out of loop
-//continue; jump back up done with this iteration
+//combine with and or or (does both of the above in one phrase
+while(isNaN(min)|| min==""){
+    min = prompt("please enter a number");
+}
+
+var max = prompt("Please enter a max number");
+
+while(isNaN(max) || max ==""){
+    min = prompt("please enter a number");
+}
+//call our function
+randomizer(5,20);
+//holds it
+var result = randomizer(min, max);
+
+console.log(result);
+
+//function to generate a random number between the min and max
+function randomizer(minNumber, maxNumber){
+  var randomNumber = Math.round(Math.random() * (maxNumber - minNumber)+ minNumber);
+  return randomNumber;
+}
+
+//create a loop to generate 15 random numbers from our function
+
+
+for(var i = 0; i<15; i++){
+   console.log(randomizer(min,max));
+}
