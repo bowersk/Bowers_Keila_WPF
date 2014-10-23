72529100d71cc77b3ddc4edf188697584667ab97
bowersk <kdbowers@fullsail.edu>
2014-10-23 18:15:59 -0400

    Entered in comments for half of the text-cir
    Circumference Problem

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,14 +8,18 @@
 //Circumference - Calculate the circumference of a circle.
 //Circumference = pi 3.14 * diameter of the circle
 
+//Asks user for radius
 var radius = prompt("Please enter the radius of your circle");
+//Prints radius to console so teacher can view
 console.log(radius);
-
+//create variable for pi
 var pi = Math.PI;
-var result = calculateArea(radius, pi);
+//var result collects the return answer from function. the calculateCir is the argument for the function that matches with its parameters.
+var result = calculateCir(radius, pi);
+//prints result variable to console in a string
 console.log("The circumference of the circle is "  + result + ".");
 
-function calculateArea(r, p )
+function calculateCir(r, p )
 {
     return r * p;
 }
