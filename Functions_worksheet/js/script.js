58295af2e41b9921d04c1ca55dd7514a8064cd2e
bowersk <kdbowers@fullsail.edu>
2014-10-23 18:28:06 -0400

    Using wrong formula for Cir +Corrected it

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -6,14 +6,14 @@
  */
 
 //Circumference - Calculate the circumference of a circle.
-//Circumference = pi 3.14 * diameter of the circle
+//Circumference = 2 * pi 3.14 * diameter of the circle
 
 //Asks user for radius
 var radius = prompt("Please enter the radius of your circle");
 //Prints radius to console so teacher can view
 console.log(radius);
 //create variable for pi
-var pi = Math.PI;
+var pi = 2 * Math.PI;
 //var result collects the return answer from function. the calculateCir is the argument for the function that matches with its parameters.
 var result = calculateCir(radius, pi);
 //prints result variable to console in a string
