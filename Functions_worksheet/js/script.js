c4120fae9424fbe3da16be01416b91ba7d4c493e
bowersk <kdbowers@fullsail.edu>
2014-10-23 18:06:09 -0400

    Solving problem - Circumference

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -4,3 +4,16 @@
  Functions_worksheet
  October 23, 2014
  */
+
+//Circumference - Calculate the circumference of a circle.
+//Circumference = pi 3.14 * diameter of the circle
+
+var radius = 5;
+var pi = Math.PI;
+var result = calculateArea(radius, pi);
+console.log("The circumference of the circle is "  + result + ".");
+
+function calculateArea(r, p )
+{
+    return r * p;
+}
