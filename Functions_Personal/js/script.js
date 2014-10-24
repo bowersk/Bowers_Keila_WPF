ef8d136af15d68a7c8e0efe9fc04495c48efb103
bowersk <kdbowers@fullsail.edu>
2014-10-23 21:23:35 -0400

    Added text to con log + check browser +completed

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -11,9 +11,13 @@
 alert("It's that time of year again! Which means it is time to figure out your Christmas Budget. Please follow along with the prompts to figure out how much per child you can spend and how many toys that will buy based on average cost.")
 
 var totalBudget = prompt("How much money can you devote to Christmas this year?");
+console.log(totalBudget + " Total Budget");
 var totalChildren = prompt("How many children do you have to shop for?");
+console.log(totalChildren + " Total Children");
 var totalOthers = prompt("How much do you wish to use for other people?");
+console.log(totalOthers + " Other people to budget for");
 var averageCostToy = 55;
+console.log(averageCostToy + " Average cost of popular toys");
 
 var resultPerChild = amountPerChild(totalBudget,totalOthers, totalChildren);
 alert("You will be able to spend " + resultPerChild + " dollars on each child. This will leave " + totalOthers + " dollars to spend on other people.");
@@ -31,10 +35,13 @@ var toysPerChild = function(perChild, toyCost){
 }
 
 var toysChild = toysPerChild(resultPerChild, averageCostToy);
-console.log(toysChild);
+alert("At this rate you will be able to by each child " + toysChild + " toys.");
+console.log("At this rate you will be able to by each child " + toysChild + " toys.");
 
-console.log(totalBudget + " Total Budget");
-console.log(totalChildren + " Total Children");
-console.log(totalOthers + " Other people to budget for");
-console.log(averageCostToy + " Average cost of popular toys");
+
+//completed
+
+
+
+
 
