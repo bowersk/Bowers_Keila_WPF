0c1a069464ced5be896e04eec49dc342980f8b92
bowersk <kdbowers@fullsail.edu>
2014-10-23 21:59:02 -0400

    added else if statement and logical opp

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -13,6 +13,23 @@ var hours = prompt("Estimate how many hours the project will take to complete.")
 var hourlyRate = 20;
 var discountHourlyRate = 18;
 
+var discountPrice = discountPrice(hours,discountHourlyRate);
+var normalPrice = normalPrice(hours,hourlyRate);
+
+
+
+if(hours <= 75){
+    function normalPrice(h,hRate){
+        return h * hRate
+    }
+    console.log(normalPrice);
+}else if(hours > 75){
+    function discountPrice(h2,dRate){
+        return h2 * dRate
+    }
+    console.log(discountPrice);
+}
+
 
 
 
