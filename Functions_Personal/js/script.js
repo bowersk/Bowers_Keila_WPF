85a3fe7d8590b38b729352492b0a09ce69bd602c
bowersk <kdbowers@fullsail.edu>
2014-10-23 20:16:15 -0400

    Created function with 3 para

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -9,10 +9,12 @@
 
 var totalBudget = prompt("How much money can you devote to Christmas this year?");
 var totalChildren = prompt("How many children do you have to shop for?");
+var totalOthers = prompt("How much do you wish to use for other people?");
 
-var resultPerChild = amountPerChild(totalBudget, totalChildren);
-console.log("You will be able to spend " + resultPerChild + " dollars on each child.");
+var resultPerChild = amountPerChild(totalBudget,totalOthers, totalChildren);
+console.log("You will be able to spend " + resultPerChild + " dollars on each child. This will leave " + totalOthers + " dollars to spend on other people.");
 
-function amountPerChild(budget, children){
-    return budget/children
+function amountPerChild(budget, others, children){
+    return (budget - others )/children
 }
+
