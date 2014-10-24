bf66d8aff325fd74cb5c5a52c4747ac63283b9d7
bowersk <kdbowers@fullsail.edu>
2014-10-23 22:24:44 -0400

    added in function for cooking time

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -8,6 +8,16 @@
 //What temperature will your hot pockets be at when you cook them?
 //Cooking time / number of hot pockets = temperature level (Base level will be 2.50 recommended for 1 hot pocket)
 
+var baseTemp = 2.50 / 1;
+var cookingTime = prompt("Please enter counting time in decimal format.");
+var pocketNumber = prompt("How many Hot Pockets are you cooking at one time?");
+
+var pocketTemp = pocketTemp(cookingTime, pocketNumber);
+console.log(pocketTemp);
+
+function pocketTemp(time, number){
+    return time / number
+}
 
 
 
