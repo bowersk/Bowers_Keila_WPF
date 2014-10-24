7736017f9d5497cfab28b5435d5d29367cb4c21c
bowersk <kdbowers@fullsail.edu>
2014-10-23 20:41:37 -0400

    Added else if statement

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -10,6 +10,8 @@
 var totalBudget = prompt("How much money can you devote to Christmas this year?");
 var totalChildren = prompt("How many children do you have to shop for?");
 var totalOthers = prompt("How much do you wish to use for other people?");
+var shoppingYet = prompt("Have you finished shopping yet? \nEnter Y for Yes and N for N");
+
 
 var resultPerChild = amountPerChild(totalBudget,totalOthers, totalChildren);
 console.log("You will be able to spend " + resultPerChild + " dollars on each child. This will leave " + totalOthers + " dollars to spend on other people.");
@@ -18,3 +20,10 @@ function amountPerChild(budget, others, children){
     return (budget - others )/children
 }
 
+if (shoppingYet === "Y"){
+    var shoppingTotal = prompt("How much money did you spend?");
+}else if(shoppingYet === "N"){
+    alert("Good luck shopping this Christmas season.");
+}else{
+    var shoppingYet = prompt("Please enter 'Y' for Yes or 'N' for no to proceed.");
+}
