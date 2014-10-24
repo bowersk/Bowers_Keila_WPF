9b2f4a78d27d75f5e64a2abee1d9c5e94db5d990
bowersk <kdbowers@fullsail.edu>
2014-10-23 21:01:24 -0400

    Changed if to annon. function

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -10,8 +10,7 @@
 var totalBudget = prompt("How much money can you devote to Christmas this year?");
 var totalChildren = prompt("How many children do you have to shop for?");
 var totalOthers = prompt("How much do you wish to use for other people?");
-var shoppingYet = prompt("Have you finished shopping yet? \nEnter Y for Yes and N for N");
-
+var averageCostToy = 55;
 
 var resultPerChild = amountPerChild(totalBudget,totalOthers, totalChildren);
 console.log("You will be able to spend " + resultPerChild + " dollars on each child. This will leave " + totalOthers + " dollars to spend on other people.");
@@ -20,10 +19,13 @@ function amountPerChild(budget, others, children){
     return (budget - others )/children
 }
 
-if (shoppingYet === "Y"){
-    var shoppingTotal = prompt("How much money did you spend?");
-}else if(shoppingYet === "N"){
-    alert("Good luck shopping this Christmas season.");
-}else{
-    var shoppingYet = prompt("Please enter 'Y' for Yes or 'N' for no to proceed.");
+var toysPerChild = function(perChild, toyCost){
+    var toys = perChild / toyCost;
+    return toys;
+
 }
+
+var toysChild = toysPerChild(resultPerChild, averageCostToy);
++console.log(toysChild);
+
+
